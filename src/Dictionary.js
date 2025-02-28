f429580cebb38e26b489930e5b719f0cdbc18abc
LJ-Fryer <ljf_aus@hotmail.com>
2025-02-28 16:56:10 +1100

    Moved api call back into dictionary component.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
+
 import Search from "./Search";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [dictionaryData, setDictionaryData] = useState(null);
+
+  function handleResponse(response) {
+    setDictionaryData(response.data);
+    console.log(response.data);
+  }
+
+  function apiCall() {
+    const apiKey = "2394045tebc4f1e4ea3820407e9abod9";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    apiCall();
+  }
 
   function handleKeywordChange(event) {
-    event.preventDefault();
     setKeyword(event.target.value);
   }
 
-  function search() {
-    alert(`Searching for ${keyword}`);
-  }
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
+      <form onSubmit={handleSubmit}>
         <input
           type="search"
           autoFocus={true}
@@ -25,7 +40,7 @@ export default function Dictionary() {
         />
         <input type="submit" value="Let's go" className="submitButton" />
       </form>
-      <Search searchWord="mask" />
+      <Search results={dictionaryData} />
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,47 +1,25 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import "./Search.css";
 
 export default function Search(props) {
-  const [dictionaryData, setDictionaryData] = useState({});
-  const [loaded, setLoaded] = useState(false);
+  console.log(props.userInput);
 
-  useEffect(() => {
-    setLoaded(false);
-  }, [props.searchWord]);
-
-  function apiCall() {
-    let dummyWord = "flurry";
-    const apiKey = "2394045tebc4f1e4ea3820407e9abod9";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${dummyWord}&key=${apiKey}`;
-
-    axios.get(apiUrl).then(handleResponse);
-  }
-
-  function handleResponse(response) {
-    setLoaded(true);
-
-    setDictionaryData({
-      word: response.data.word,
-      phonetic: response.data.phonetic,
-      category: response.data.meanings[0].partOfSpeech,
-      definition: response.data.meanings[0].definition,
-    });
-  }
-
-  if (loaded) {
+  if (props.results) {
     return (
       <div className="Search">
-        <h3>{dictionaryData.word}</h3>
-        <p className="phoneticDisplay">/{dictionaryData.phonetic}/</p>
+        <h3>{props.results.word}</h3>
+        <p className="phoneticDisplay">/{props.results.phonetic}/</p>
         <div className="definitionBox">
-          <p className="categoryDisplay mb-0">{dictionaryData.category}</p>
-          <p className="definitionDisplay">{dictionaryData.definition}</p>
+          <p className="categoryDisplay mb-0">
+            {props.results.meanings[0].partOfSpeech}
+          </p>
+          <p className="definitionDisplay">
+            {props.results.meanings[0].definition}
+          </p>
         </div>
       </div>
     );
   } else {
-    apiCall();
     return null;
   }
 }
