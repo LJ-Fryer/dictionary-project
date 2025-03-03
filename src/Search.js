e1abe5a620c57d7c19c34c73f17402c62dd08e2d
LJ-Fryer <ljf_aus@hotmail.com>
2025-03-03 11:58:24 +1100

    Added synonyms section with dummy text and styled.

--- src/Search.js
+++ src/Search.js
@@ -10,7 +10,7 @@ export default function Search(props) {
       <div className="Search">
         <h3>{props.results.word}</h3>
         <p className="phoneticDisplay">/{props.results.phonetic}/</p>
-        <div className="definitionBox">
+        <div className="definitionBox mb-2">
           {props.results.meanings.map(function (meaning, index) {
             return (
               <div key={index}>
@@ -18,6 +18,16 @@ export default function Search(props) {
               </div>
             );
           })}
+        </div>
+        <div className="synonymsBox">
+          <h5 className="text-center">Similar words</h5>
+          <div className="d-flex justify-content-evenly">
+            <span>stroll</span>
+            <span>saunter</span>
+            <span>amble</span>
+            <span>hike</span>
+            <span>stride</span>
+          </div>
         </div>
       </div>
     );
