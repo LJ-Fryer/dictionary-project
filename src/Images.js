34d17949d12814d3cbc8b5a8ed7d09f9879ec65a
LJ-Fryer <ljf_aus@hotmail.com>
2025-03-06 13:15:27 +1100

    Added anchor link to images. Added conditional rendering for unknown keyword

--- src/Images.js
+++ src/Images.js
@@ -7,12 +7,18 @@ export default function Images(props) {
       <div className="Images d-flex mt-5 me-2 ms-2 justify-content-between">
         {props.images.slice(0, 3).map(function (image, index) {
           return (
-            <img
-              src={image.src.original}
-              key={index}
-              alt={image.alt}
-              className="image-fluid"
-            />
+            <a
+              href={image.src.original}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={image.src.original}
+                key={index}
+                alt={image.alt}
+                className="image-fluid"
+              />
+            </a>
           );
         })}
       </div>
--- src/Search.js
+++ src/Search.js
@@ -5,34 +5,40 @@ import "./Search.css";
 
 export default function Search(props) {
   if (props.results) {
-    return (
-      <div className="Search">
-        <h3>{props.results.word}</h3>
-        <p className="mt-2 mb-2">/{props.results.phonetic}/</p>
-        <section>
-          {props.results.meanings.map(function (meaning, index) {
-            return (
-              <div key={index} className="definitionBox mb-2">
-                <Meaning meaning={meaning} />
-              </div>
-            );
-          })}
-        </section>
-
-        <section>
-          <h5 className="mt-3 mb-1 ms-1">Synonyms</h5>
-          <div className="synonymsBox d-flex">
+    if (props.results.status) {
+      return (
+        <div className="text-danger">Oops, can't find that. Try again</div>
+      );
+    } else {
+      return (
+        <div className="Search">
+          <h3>{props.results.word}</h3>
+          <p className="mt-2 mb-2">/{props.results.phonetic}/</p>
+          <section>
             {props.results.meanings.map(function (meaning, index) {
               return (
-                <div key={index}>
-                  <Synonyms synonyms={meaning.synonyms} />
+                <div key={index} className="definitionBox mb-2">
+                  <Meaning meaning={meaning} />
                 </div>
               );
             })}
-          </div>
-        </section>
-      </div>
-    );
+          </section>
+
+          <section>
+            <h5 className="mt-3 mb-1 ms-1">Synonyms</h5>
+            <div className="synonymsBox d-flex flex-wrap">
+              {props.results.meanings.map(function (meaning, index) {
+                return (
+                  <div key={index}>
+                    <Synonyms synonyms={meaning.synonyms} />
+                  </div>
+                );
+              })}
+            </div>
+          </section>
+        </div>
+      );
+    }
   } else {
     return null;
   }
