8249681b5a9abfa8ed555b3ff35bf9b28d4251fb
LJ-Fryer <ljf_aus@hotmail.com>
2025-03-06 17:51:43 +1100

    Added list element to Meaning component, and styled.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <main>
             <h1>The Vocabulator</h1>
             <h5 className="mb-4">Okay, it's a dictionary 🔍</h5>
-            <Dictionary defaultKeyword="nonsense" />
+            <Dictionary defaultKeyword="funny" />
           </main>
           <footer>
             <small>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
-    <div className="Meaning ms-1">
-      <p>{props.meaning.definition}</p>
-    </div>
+    <ul className="Meaning ms-1">
+      <li>{props.meaning.definition}</li>
+    </ul>
   );
 }
